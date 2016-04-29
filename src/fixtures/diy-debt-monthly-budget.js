e0e2d6cdbae6c1078f6fb821701a67b01469985c
Evan Hobbs <ehobbs@nerdwallet.com>
2016-04-29 12:08:52 -0700

    Adds compliance footer to unsubscribe

--- src/fixtures/diy-debt-monthly-budget.js
+++ src/fixtures/diy-debt-monthly-budget.js
@@ -1,4 +1,6 @@
 module.exports = {
   first_name: 'Evan',
-  month: 'February'
+  month: 'February',
+  notification_type: 'monthly_budget',
+  unsubscribe_token: '45dcaa50ff3d58bef9a596a34215bd171719d5983535820cc56ec0f1df130746'
 }
--- src/fixtures/diy-debt-payment-reminder.js
+++ src/fixtures/diy-debt-payment-reminder.js
@@ -3,5 +3,7 @@ module.exports = {
     { name: 'Test Account' },
     { name: 'Test Account' },
     { name: 'Test Account' }
-  ]
+  ],
+  notification_type: 'payment_reminder',
+  unsubscribe_token: '45dcaa50ff3d58bef9a596a34215bd171719d5983535820cc56ec0f1df130746'
 }
